d1149c56ef4bfe4da02c76dbec8c97c699903e43
vatanapm-tech <vatanapm@union.edu>
2026-02-10 12:17:51 -0500

    First commit (2/10)

---
+++ main/main.c
@@ -0,0 +1,217 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <sys/time.h>
+#include <hd44780.h>
+#include <esp_idf_lib_helpers.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+#include "stdio.h"
+#include "esp_adc/adc_cali.h"
+#include "freertos/timers.h"
+
+
+#define PSEAT_PIN         GPIO_NUM_4       // passenger seat button pin 4
+#define DSEAT_PIN         GPIO_NUM_5       // driver seat button pin 5
+#define PBELT_PIN         GPIO_NUM_6       // passenger belt switch pin 6
+#define DBELT_PIN         GPIO_NUM_7       // driver belt switch pin 7
+#define IGNITION_BUTTON   GPIO_NUM_17      // ignition button pin 17
+#define READY_LED         GPIO_NUM_8       // ready LED pin 8
+#define SUCCESS_LED       GPIO_NUM_10      // success LED pin 10
+#define ALARM_PIN         GPIO_NUM_12      // alarm pin 12
+#define ADC_ATTEN         ADC_ATTEN_DB_12  // set ADC attenuation
+#define BITWIDTH          ADC_BITWIDTH_12  // set ADC bitwidth
+
+
+bool dseat = false;     // Detects when the driver is seated 
+bool pseat = false;     // Detects when the passenger is seated
+bool dbelt = false;     // Detects when the driver seatbelt is on
+bool pbelt = false;     // Detects when the passenger seatbelt is on
+bool ignition = false;  // Detects when the ignition is turned on
+int executed = 0;       // Keep track of print statements
+int ready_led = 0;      // Keep track of whether ready_led should be on or off
+int ignition_off = 0;   // Keep track of whether the ignition can be turned off
+
+static uint32_t get_time_sec()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
+
+static const uint8_t char_data[] =
+{
+    0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
+    0x1f, 0x11, 0x0a, 0x04, 0x0a, 0x11, 0x1f, 0x00
+};
+
+void lcd_test(void *pvParameters)
+{
+    hd44780_t lcd =
+    {
+        .write_cb = NULL,
+        .font = HD44780_FONT_5X8,
+        .lines = 2,
+        .pins = {
+            .rs = GPIO_NUM_38,
+            .e  = GPIO_NUM_37,
+            .d4 = GPIO_NUM_36,
+            .d5 = GPIO_NUM_35,
+            .d6 = GPIO_NUM_48,
+            .d7 = GPIO_NUM_47,
+            .bl = HD44780_NOT_USED
+        }
+    };
+
+    ESP_ERROR_CHECK(hd44780_init(&lcd));
+
+    hd44780_upload_character(&lcd, 0, char_data);
+    hd44780_upload_character(&lcd, 1, char_data + 8);
+
+    hd44780_gotoxy(&lcd, 0, 0);
+    hd44780_puts(&lcd, "\x08 Hello, World!");
+    hd44780_gotoxy(&lcd, 0, 1);
+    hd44780_puts(&lcd, "\x09 ");
+
+    char time[16];
+
+    while (1)
+    {
+        hd44780_gotoxy(&lcd, 2, 1);
+
+        snprintf(time, 7, "%" PRIu32 "  ", get_time_sec());
+        time[sizeof(time) - 1] = 0;
+
+        hd44780_puts(&lcd, time);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(lcd_test, "lcd_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+
+    // set driver seat pin config to input and internal pullup
+    gpio_reset_pin(DSEAT_PIN);
+    gpio_set_direction(DSEAT_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(DSEAT_PIN);
+
+    // set passenger seat pin config to input and internal pullup
+    gpio_reset_pin(PSEAT_PIN);
+    gpio_set_direction(PSEAT_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(PSEAT_PIN);
+
+    // set driver belt pin config to input and internal pullup
+    gpio_reset_pin(DBELT_PIN);
+    gpio_set_direction(DBELT_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(DBELT_PIN);
+
+    // set passenger belt pin config to input and internal pullup
+    gpio_reset_pin(PBELT_PIN);
+    gpio_set_direction(PBELT_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(PBELT_PIN);
+
+    // set ignition button config to input and internal pullup
+    gpio_reset_pin(IGNITION_BUTTON);
+    gpio_set_direction(IGNITION_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(IGNITION_BUTTON);
+
+    // set ready led pin config to output, level 0
+    gpio_reset_pin(READY_LED);
+    gpio_set_direction(READY_LED, GPIO_MODE_OUTPUT);
+
+    // set success led pin config to output, level 0
+    gpio_reset_pin(SUCCESS_LED);
+    gpio_set_direction(SUCCESS_LED, GPIO_MODE_OUTPUT);
+
+    // set alarm pin config to output, level 0
+    gpio_reset_pin(ALARM_PIN);
+    gpio_set_direction(ALARM_PIN, GPIO_MODE_OUTPUT);
+
+    while (1){
+
+        // Task Delay to prevent watchdog
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+
+        // initialize variables in relation to GPIO pin inputs
+        dseat = gpio_get_level(DSEAT_PIN)==0;
+        pseat = gpio_get_level(PSEAT_PIN)==0;
+        dbelt = gpio_get_level(DBELT_PIN)==0;
+        pbelt = gpio_get_level(PBELT_PIN)==0;
+        ignition = gpio_get_level(IGNITION_BUTTON)==0;
+
+        // if the driver seat button is pressed, print the welcome message once
+        if (dseat){
+            if (executed == 0){     // if executed equals 0, print welcome message
+                printf("Welcome to enhanced alarm system model 218-W25 \n"); 
+                executed = 1;       // set executed = 1 so welcome message only prints once
+            }
+        }
+
+        // if all of the conditions are met
+        if (dseat && pseat && dbelt && pbelt){
+            //set ready led to ON
+            if (executed == 1 && ready_led == 0){
+                gpio_set_level(READY_LED, 1);
+                ready_led = 1;
+            }
+            // if ignition button is pressed while all conditions are met
+            if (ignition == true && executed == 1){
+                // turn on ignition LED and turn off ready LED
+                gpio_set_level(SUCCESS_LED, 1);
+                gpio_set_level(READY_LED, 0);
+                gpio_set_level(ALARM_PIN, 0);
+                // print engine started message once
+                printf("Engine started!\n");
+                executed = 2;       // set executed = 2 so engine started message only prints once
+            }
+        }
+            
+        // otherwise (at least one condition is not satisfied)
+        else{
+            // set ready LED to OFF and set variable ready_led to 0
+            gpio_set_level(READY_LED,0);
+            ready_led = 0;
+            // if ignition button is pressed while conditions are not satisfied
+            if (ignition==true && executed < 2){
+                    // turn on alarm buzzer
+                    gpio_set_level(ALARM_PIN, 1);
+                    printf("Ignition inhibited.\n");
+                    // check which conditions are not met, print corresponding message
+                    if (!pseat){
+                        printf("Passenger seat not occupied.\n");
+                    }
+                    if (!dseat){
+                        printf("Driver seat not occupied.\n");
+                    }
+                    if (!pbelt){
+                        printf("Passenger seatbelt not fastened.\n");
+                    }
+                    if (!dbelt){
+                        printf("Drivers seatbelt not fastened.\n");
+                    }
+                    executed = 4;
+                
+            }
+        }
+
+         // if executed = 4 (failed ignition) and ignition button is released
+        if (ignition == false && executed == 4){
+            // reset to state after welcome message, testing for conditions
+            executed = 1;
+        }
+        // if ignition is successfully started and then ignition is released, set ignition_off = 1
+        if (executed == 2 && ignition == false){
+            ignition_off = 1;
+        }
+
+        // if ignition_off = 1 and inition is pressed, turn off all LEDs
+        if (ignition_off==1 && ignition == true){
+            gpio_set_level(SUCCESS_LED,0);          // turn off ignition LED
+            executed = 3;                           // set executed = 3 to keep LEDs off
+        }
+    }
+}
